26793779eda0a23966c16a2a4c27668b4db14747
Raul Escobar <raulescobar13@gmail.com>
2017-04-30 19:23:25 -0300

    Adds save button to player-selection

--- src/components/player-selection.js
+++ src/components/player-selection.js
@@ -1,20 +1,31 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { updatePlayerSelection } from '../actions';
-import { getPositionSelected, getPlayers } from '../reducers';
+import {
+    updatePlayerSelection,
+    positionSelected
+} from '../actions';
+import {
+    getPositionSelected,
+    getPlayers
+} from '../reducers';
+
+const positions = ['ARQUERO','DEFENSA','MEDIOCAMPO', 'DELANTEROS', '11 IDEAL'];
 
 class PlayerSelection extends Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            players: this.props.players
+            players: this.props.players,
+            saveDisabled: true
         }
     }
 
     componentWillReceiveProps(nextProps) {
         if (nextProps.positionSelected !== this.props.positionSelected) {
             this.setState({ players: nextProps.players });
+            this.setState({ saveDisabled: true });
         }
     }
 
@@ -41,24 +52,33 @@ class PlayerSelection extends Component {
             return Object.assign({}, this.state, { players: this.selectPlayer(player) } );
         });
 
+        this.setState({ saveDisabled: false });
         this.props.updatePlayerSelection(this.state.players);
     }
 
     renderPlayers() {
         const players = Object.keys(this.state.players);
-        console.log('KEYS', players);
         return players.map( (player, index) => {
             const className = this.playerSelectedHighlight(player) ? 'player-selected' : 'player';
             return <div key={index} className={className} onClick={() => this.clickHandler(player)}>{this.state.players[player].name}</div>;
             }
         );
+    }
 
+    saveHandler() {
+        let selectedIndex = positions.indexOf(this.props.positionSelected);
+        selectedIndex++;
+        this.props.updatePositionSelected(positions[selectedIndex]);
     }
 
     render() {
+        const buttonClass = this.state.saveDisabled ? 'btn btn-default disabled' : 'btn btn-default';
         return (
             <div className="player-selection">
                 {this.renderPlayers()}
+                <div className="save-button">
+                    <button className={buttonClass} onClick={() => this.saveHandler()}>{'Guardar'}</button>
+                </div>
             </div>
         );
     }
@@ -75,7 +95,8 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = dispatch => {
     return {
-        updatePlayerSelection: selection => dispatch(updatePlayerSelection(selection))
+        updatePlayerSelection: selection => dispatch(updatePlayerSelection(selection)),
+        updatePositionSelected: position => dispatch(positionSelected(position))
     }
 }
 export default connect(mapStateToProps, mapDispatchToProps)(PlayerSelection);
