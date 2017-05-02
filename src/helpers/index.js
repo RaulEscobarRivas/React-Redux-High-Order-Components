4717103295a88c428a0b3a3e220caba0ece4809d
Raul Escobar <raulescobar13@gmail.com>
2017-05-02 00:27:08 -0300

    getSelectedPlayers selector WIP

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -2,7 +2,7 @@ const getSelectedItems = players => (
     Object.keys(players).map( position => {
         for (const player in players[position]) {
             if (players[position][player].selected === true ) {
-                return players[position][player].name;
+                return Object.assign({},  players[position] , players[position][player].name);
             }
         }
     })
